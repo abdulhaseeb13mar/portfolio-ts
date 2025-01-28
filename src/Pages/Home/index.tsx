b548eb3adbbdc5dad996f25663c3eb4c405c0719
abdulhaseeb13mar <abdulhaseeb13mar@gmail.com>
2025-01-29 01:10:39 +0500

    migration: refactor Home component to remove makeStyles and implement sx prop for Avatar styling

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Grid2 as Grid, Avatar, Typography } from "@mui/material";
-import { Theme } from "@mui/material/styles";
 import HomeSummary from "./homeSummary";
-import { makeStyles } from "@mui/styles";
 import ME from "Assets/me.png";
 import "./Home.scss";
 import { dispatch, useSelector } from "Store";
@@ -10,41 +8,22 @@ import { setShowHeader } from "Store/Slices/app";
 import { Fade } from "react-awesome-reveal";
 import { Zoom } from "react-awesome-reveal";
 
-const useStyles = makeStyles((theme: Theme) => ({
-  large: {
-    width: 30,
-    // width: theme.spacing(30),
-    // height: theme.spacing(30),
-    height: 30,
-    boxShadow: "0px 0px 18px 8px black",
-    transition: "0.2s ease-in-out",
-    cursor: "pointer",
-    "&:hover": {
-      // width: theme.spacing(35),
-      // height: theme.spacing(35),
-      width: 35,
-      height: 35,
-    },
-  },
-}));
-
 const Home: React.FC = () => {
-  useEffect(() => {
-    makeTrue();
-  }, []);
+  // useEffect(() => {
+  //   makeTrue();
+  // }, []);
 
-  const [showStarter, setShowStarter] = useState(false);
+  // const [showStarter, setShowStarter] = useState(false);
   const showHeader = useSelector((state) => state.app.showHeader);
-  const classes = useStyles();
 
-  const makeTrue = () => {
-    setTimeout(() => {
-      setShowStarter(true);
-    }, 200);
-  };
+  // const makeTrue = () => {
+  //   setTimeout(() => {
+  //     setShowStarter(true);
+  //   }, 200);
+  // };
 
   const handleToggleShow = () => {
-    setShowStarter(false);
+    // setShowStarter(false);
     dispatch(setShowHeader(true));
     setTimeout(() => {
       (
@@ -73,11 +52,26 @@ const Home: React.FC = () => {
       <Zoom
         direction="right"
         cascade
+        delay={200}
         // when={showStarter}
         //  collapse
       >
         <div className="home-centered-div" onClick={handleToggleShow}>
-          <Avatar alt="Me" src={ME} className={classes.large} />
+          <Avatar
+            alt="Me"
+            src={ME}
+            sx={(theme) => ({
+              width: theme.spacing(30),
+              height: theme.spacing(30),
+              boxShadow: "0px 0px 18px 8px black",
+              transition: "0.2s ease-in-out",
+              cursor: "pointer",
+              "&:hover": {
+                width: theme.spacing(35),
+                height: theme.spacing(35),
+              },
+            })}
+          />
 
           <Typography variant="h3" className="home-text1">
             Hey! I am <br /> Abdul Haseeb
