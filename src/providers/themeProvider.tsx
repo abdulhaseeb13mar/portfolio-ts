d27d924f5e854a60c72b0d7c2f77053628771cde
abdulhaseeb13mar <abdulhaseeb13mar@gmail.com>
2025-01-29 01:09:47 +0500

    migration: update theme provider to use dark mode with custom palette

--- src/providers/themeProvider.tsx
+++ src/providers/themeProvider.tsx
@@ -2,7 +2,13 @@ import { createTheme } from "@mui/material/styles";
 import { ThemeProvider } from "@mui/styles";
 import { ReactNode } from "react";
 
-const theme = createTheme();
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: { main: "#f74458" },
+    secondary: { main: "#f74458" },
+  },
+});
 
 interface ThemeProviderCompProps {
   children: ReactNode;
