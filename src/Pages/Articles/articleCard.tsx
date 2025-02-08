e0143e94b5961a47c6c82ff03c43a7ebc8e3cdb1
abdulhaseeb13mar <abdulhaseeb13mar@gmail.com>
2025-02-09 03:56:22 +0500

    migration: enhance ArticleCard component with loading state and skeletons

--- src/Pages/Articles/articleCard.tsx
+++ src/Pages/Articles/articleCard.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { makeStyles } from "@mui/styles";
 import {
   Card,
@@ -6,6 +6,7 @@ import {
   CardContent,
   CardMedia,
   Typography,
+  Skeleton,
 } from "@mui/material";
 import dummyMedium from "Assets/Articles/medium.png";
 import axios from "axios";
@@ -23,22 +24,9 @@ interface Article {
 const ArticleCard: React.FC = () => {
   const classes = useStyles();
   const [articlesList, setArticlesList] = useState<Article[]>([]);
+  const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    axios({
-      method: "get",
-      url: "https://v1.nocodeapi.com/haseeb13mar/medium/qjiKFKVYmeOHMapI",
-      params: {},
-    })
-      .then(function (response) {
-        handleArticlesResponse(response.data);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }, []);
-
-  const handleArticlesResponse = (articles: any[]) => {
+  const handleArticlesResponse = useCallback((articles: any[]) => {
     let prepareArticles: Article[] = [];
     articles.forEach((article) => {
       const img = findFirstImgURL(article.content);
@@ -51,7 +39,23 @@ const ArticleCard: React.FC = () => {
       });
     });
     setArticlesList(prepareArticles);
-  };
+  }, []);
+
+  useEffect(() => {
+    axios({
+      method: "get",
+      url: "https://v1.nocodeapi.com/haseeb13mar/medium/qjiKFKVYmeOHMapI",
+      params: {},
+    })
+      .then(function (response) {
+        handleArticlesResponse(response.data);
+        setLoading(false);
+      })
+      .catch(function (error) {
+        console.log(error);
+        setLoading(false);
+      });
+  }, [handleArticlesResponse]);
 
   const handlePhotosLink = (url: string) => {
     window.open(url);
@@ -64,40 +68,68 @@ const ArticleCard: React.FC = () => {
     return match ? match[1] : null;
   }
 
+  // Render skeleton cards if loading.
+  const renderSkeletons = () => {
+    // Change the number of skeleton cards as needed
+    const skeletonArray = Array.from(new Array(3));
+    return skeletonArray.map((_, index) => (
+      <Card key={index} className={classes.root}>
+        <CardActionArea>
+          <Skeleton
+            variant="rectangular"
+            className={classes.media2}
+            sx={{ height: 200 }}
+          />
+          <CardContent>
+            <Skeleton variant="text" height={30} />
+            <Skeleton variant="text" height={20} width="300px" />
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    ));
+  };
+
   return (
     <div className="article-card-container">
-      {articlesList.map((article, key) => {
-        return (
-          <Zoom direction="right" key={key}>
-            <Card
-              className={classes.root}
-              onClick={() => handlePhotosLink(article.url)}
-            >
-              <CardActionArea>
-                <CardMedia
-                  className={classes.media2}
-                  image={article.img}
-                  title="Contemplative Reptile"
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    {article.title}
-                  </Typography>
-                  <div className="r-c-fs article-categories-container">
-                    {article.categories.map((category, key) => {
-                      return (
+      {loading
+        ? renderSkeletons()
+        : articlesList.map((article, key) => (
+            <Zoom direction="right" triggerOnce key={key}>
+              <Card
+                sx={{
+                  bgcolor: "#424242",
+                  color: "white",
+                  maxWidth: 500,
+                  marginBottom: 2,
+                }}
+                // className={classes.root}
+                onClick={() => handlePhotosLink(article.url)}
+              >
+                <CardActionArea>
+                  <CardMedia
+                    className={classes.media2}
+                    image={article.img}
+                    title={article.title}
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {article.title}
+                    </Typography>
+                    <div
+                      className="r-c-fs article-categories-container"
+                      style={{ display: "flex", alignItems: "center" }}
+                    >
+                      {article.categories.map((category, key) => (
                         <div className="article-single-category" key={key}>
                           #{category}
                         </div>
-                      );
-                    })}
-                  </div>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          </Zoom>
-        );
-      })}
+                      ))}
+                    </div>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            </Zoom>
+          ))}
     </div>
   );
 };
