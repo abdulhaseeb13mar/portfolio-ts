81ab7061442eb6f0985f5a468c4ac985d1202beb
abdulhaseeb13mar <abdulhaseeb13mar@gmail.com>
2026-02-02 15:50:50 +0500

    feat: implement RootListener for smooth scrolling and route change handling

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,41 +1,13 @@
-import { useEffect, useRef } from "react";
-
 import { Footer } from "../components/Footer";
 import { Navbar } from "../components/Navbar";
-import Lenis from "@studio-freight/lenis";
+import RootListener from "@/listeners/rootListener";
 import { Outlet } from "@tanstack/react-router";
 
-// import { ScrollProgress } from '../components/ScrollProgress'; // To be implemented
-
 export function RootLayout() {
-  const lenisRef = useRef<Lenis | null>(null);
-
-  useEffect(() => {
-    lenisRef.current = new Lenis({
-      duration: 1.2,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      orientation: "vertical",
-      gestureOrientation: "vertical",
-      smoothWheel: true,
-      wheelMultiplier: 1,
-      touchMultiplier: 2,
-    });
-
-    function raf(time: number) {
-      lenisRef.current?.raf(time);
-      requestAnimationFrame(raf);
-    }
-
-    requestAnimationFrame(raf);
-
-    return () => {
-      lenisRef.current?.destroy();
-    };
-  }, []);
-
   return (
     <div className="flex flex-col min-h-screen bg-white dark:bg-black text-black dark:text-white antialiased">
       {/* <ScrollProgress /> */}
+      <RootListener />
       <Navbar />
       <main className="grow pt-20">
         <Outlet />
